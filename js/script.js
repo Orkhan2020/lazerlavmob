7bd1b9710f6671301e523a3528e6c830a23eea3c
Orkhan <sherisl2017@gmail.com>
2020-07-26 14:37:05 +0700

    update

--- js/script.js
+++ js/script.js
@@ -76,8 +76,8 @@ $(document).ready(function(){
 
 
 $(".tab_item").not(":first").hide();
-$(".wrapper .tab").click(function() {
-  $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+$(".epilation_zones .tab").click(function() {
+  $(".epilation_zones .tab").removeClass("active").eq($(this).index()).addClass("active");
   $(".tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
 
